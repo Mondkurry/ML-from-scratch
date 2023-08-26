4dee6038869dc30c2cb280463fee99f55cdbc29c
Aryan <aryan.mondkar@gmail.com>
2023-08-25 17:58:24 -0700

    working stuffs and some minor changes

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -1,6 +1,5 @@
 from Engine import Value, Neuron, MLP, Visualizer
-from MLUtils import loadingAnimation
-from tqdm import tqdm
+from MLUtils import printPurple
 
 # initialize a model 
 model = MLP(3, [4, 4, 1]) # 2-layer neural network with 4 neuron in each hidden layer
@@ -12,29 +11,35 @@ xs = [
     [3.0, -1.0, 0.5],
     [0.5, 1.0, 1.0],
     [1.0, 1.0, -1.0],
+    [0.25, 1.0, 1.0],
+    [0.25, 0.75, 1.0],
 ]
 
-ys = [1.0, -1.0, -1.0, 1.0]
+ys = [1.0, -1.0, -1.0, 1.0, 0.5, -0.5]
 
 # Specify Epochs and Learning Rate
-epochs = 1000
+epochs = 10
+iterPerEpoch = 100
 lr = 0.05
 
 def train():
-    for k in tqdm(range(epochs), desc=f"Training MLP", ascii=False, ncols=75):
-        ypred = [model(x) for x in xs]
-        loss = sum((yOut + (-yGroundTruth)) ** 2 for yGroundTruth, yOut in zip(ys, ypred)) # Forward Propagation
-        
-        loss.label = "Loss"
-        
-        for param in model.parameters():
-            param.grad = 0.0 # Zero the gradient
+    initialLoss = sum((yOut + (-yGroundTruth)) ** 2 for yGroundTruth, yOut in zip(ys, [model(x) for x in xs]))
+    for i in range(epochs):
+        for k in range(iterPerEpoch):
+            ypred = [model(x) for x in xs]
+            loss = sum((yOut + (-yGroundTruth)) ** 2 for yGroundTruth, yOut in zip(ys, ypred)) # Forward Propagation
             
-        loss.backProp() # Back Propagation
-        
-        for param in model.parameters():
-            param.value +=  param.grad * -lr # Update the parameters by a small factor
+            loss.label = "Loss"
             
-    print (f'Epoch {k+1} Loss: {loss.value:.6f}')
-        
+            for param in model.parameters():
+                param.grad = 0.0 # Zero the gradient
+                
+            loss.backProp() # Back Propagation
+            
+            for param in model.parameters():
+                param.value +=  param.grad * -lr # Update the parameters by a small factor
+                
+        print(f'Epoch {i+1} Loss: {loss.value:.6f}')
+    printPurple(f"\nSummary of Training: \n---------------------")
+    printPurple(f"Initial Loss: {initialLoss.value:.6f} \nFinal Loss: {loss.value:.6f} \nTotal Improvement: {(initialLoss.value - loss.value):.6f}")
 train()
--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Making a very simple Machine Learning Model from Scratch without the use of libraries. Just a fun experiment to expand my understanding.
 
 Files:
-Prototype.ipynb:    Notebook for prototyping the different classes, as well as testing features.
+mainNotebook.ipynb:         Notebook for prototyping the different classes, as well as testing features. Go to this Notebook if you want to see how I made progress towards the project
+Engine.py:                  Main Library, Collection of good code from mainNotebook.py and cleaned up all the prototype code to make it a true library
+MultiLayerPerceptron.py:    Test File for a very simple Multilayer Perceptron. Run this file for a Demo :)
+MLUtils.py:                 Just some Utils I only really ended up using the printPurple class which essentially functions like a print statement but outputs purple text
